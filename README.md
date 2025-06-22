9bbb78a94fef62099ff9fbbda7dcb714ed10231b
dharun <dharunmd05@gmail.com>
2025-06-22 18:56:02 +0530

    folder structure added

---
+++ README.md
@@ -0,0 +1,17 @@
+# End-to-End-Medical-ChatBot
+
+step 1
+'''bash 
+conda create -n medibot python=3.10 -y
+'''
+
+
+
+'''bash
+conda activate medibot
+'''
+
+step 2
+''' bash
+pip install -r requirments.txt
+'''
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='generative_ai_project',
+    version='0.0.0',
+    author='Bappy Ahmed',
+    author_email='entbappy73@gmail.com',
+    packages=find_packages(),
+    install_requires=[]  # You can fill this in or read from requirements.txt
+)
